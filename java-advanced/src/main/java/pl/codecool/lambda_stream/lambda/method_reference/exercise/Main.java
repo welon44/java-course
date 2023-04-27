6b99f1de244133c392c4760fa292bad914c6e037
Welenc Marcin <y087599@gppl.corp.local>
2023-04-27 17:18:35 +0200

    zadanie na referencje

---
+++ java-advanced/src/main/java/pl/codecool/lambda_stream/lambda/method_reference/exercise/Main.java
@@ -0,0 +1,15 @@
+package pl.codecool.lambda_stream.lambda.method_reference.exercise;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        ReferenceUtils referenceUtils = new ReferenceUtils();
+
+        System.out.println("Liczby parzyste: " + referenceUtils.getEventList());
+        System.out.println("Posortowane po długosci od najkrótszego: " + referenceUtils.sortByLength());
+        System.out.println("Suma liczb z listy: " + referenceUtils.getSum());
+        System.out.println("Maksymalna liczba z listy: " + referenceUtils.getMaxNumberFromList());
+
+    }
+}
---
+++ java-advanced/src/main/java/pl/codecool/lambda_stream/lambda/method_reference/exercise/ReferenceUtils.java
@@ -0,0 +1,66 @@
+package pl.codecool.lambda_stream.lambda.method_reference.exercise;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ReferenceUtils {
+
+    private final List<Integer> integerList = new ArrayList<>();
+    private final List<String> stringList = new ArrayList<>();
+
+    public ReferenceUtils() {
+        this.setStringList();
+        this.setIntegerList();
+    }
+
+    public void setIntegerList() {
+        for (int i = 0; i < 10; i++) {
+            integerList.add(i);
+        }
+    }
+
+    public void setStringList() {
+        stringList.add("Testaaaa");
+        stringList.add("Testa");
+        stringList.add("Testaaf");
+    }
+
+    public List<Integer> getIntegerList() {
+        return integerList;
+    }
+
+    public List<String> getStringList() {
+        return stringList;
+    }
+
+    public int getSum() {
+        return getIntegerList().stream()
+                .reduce(0, Integer::sum);
+    }
+
+    public int getMaxNumberFromList() {
+        return getIntegerList().stream()
+                .max(Integer::compareTo)
+                .orElseThrow(null);
+    }
+
+    public List<String> sortByLength() {
+        return getStringList().stream()
+                .sorted(ReferenceUtils::compareByLength).collect(Collectors.toList());
+    }
+
+    public List<Integer> getEventList() {
+        return getIntegerList().stream()
+                .filter(ReferenceUtils::isNumberEven)
+                .collect(Collectors.toList());
+    }
+
+    private static int compareByLength(String aName, String bName) {
+        return aName.length() - bName.length();
+    }
+
+    private static boolean isNumberEven(Integer number) {
+        return number % 2 == 0;
+    }
+}
